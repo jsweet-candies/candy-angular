ba9cd188c9e22c1df589f686807690308551733b
Renaud Pawlak <renaud.pawlak@gmail.com>
2017-01-03 16:54:02 +0100

    fixed packages

--- core/v1.4.x/src/main/java/def/angularjs/ng/package-info.java
+++ core/v1.4.x/src/main/java/def/angularjs/ng/package-info.java
@@ -1,4 +1,3 @@
 /** This package contains the def.angular.legacy library (source: Definitely Typed). */
-@jsweet.lang.Root(dependencies={"def.dom","def.js","def.jquery"}, mixins={def.angularjs.ng.Function.class})
 @jsweet.lang.Module("angular")
 package def.angularjs.ng;
